5b48f917b10804b46547e5a800fdae3eff0d3da9
Rahul Sonar <sonar.soni@gmail.com>
2015-06-19 02:10:00 +0530

    issue2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ This is version1 file1
 ok
 Hello1
 patch 2
-this is dev specific code, which is not in master
+this is dev specific code, which is not in master and added issue2 specific code here
 1uuu
 4  
 5   11
